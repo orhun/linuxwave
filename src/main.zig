c2b583a4a569182397208577abe6d20d9a736559
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-02-13 20:37:54 +0300

    feat: create the logic for turning arbitrary data into notes

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,25 @@ const std = @import("std");
 const text = "【ｌｉｎｕｘｗａｖｅ】";
 
 pub fn main() !void {
+    std.debug.print("{s}\n", .{text});
+
+    const stdout = std.io.getStdOut().writer();
+
+    const scale = [_]f32{ 0, 2, 4, 5, 7, 9, 11, 12 };
+    var i: f64 = 0;
+    while (i < 1) : (i += 0.0001) {
+        const note = 100 * @sin(1382 * @exp((scale['a' % 8 + 1] / 12) * @log(2.0)) * i);
+
+        try stdout.print("{d}\n", .{@floatToInt(i32, note)});
+    }
+}
+
+// const random_buffer = try generateRandom(2);
+// std.debug.print("{d}\n", .{random_buffer});
+fn generateRandom(comptime len: u8) ![]u8 {
     const file = try std.fs.openFileAbsolute("/dev/urandom", .{});
     defer file.close();
-
-    var buffer: [2]u8 = undefined;
-    const bytes_read = try file.readAll(&buffer);
-
-    std.debug.print("{d}\n", .{buffer[0..bytes_read]});
-    std.debug.print("{s}\n", .{text});
+    var buffer: [len]u8 = undefined;
+    _ = try file.readAll(&buffer);
+    return &buffer;
 }
