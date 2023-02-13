a86343653c444cbea1de5b13695409765fbbc300
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-02-13 22:37:15 +0300

    feat: encode WAV from random samples

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,38 @@
 const std = @import("std");
+const wav = @import("wav.zig");
 
 const text = "【ｌｉｎｕｘｗａｖｅ】";
 
 pub fn main() !void {
     std.debug.print("{s}\n", .{text});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var list = std.ArrayList(u8).init(allocator);
 
     const stdout = std.io.getStdOut().writer();
 
-    const scale = [_]f32{ 0, 2, 4, 5, 7, 9, 11, 12 };
-    var i: f64 = 0;
-    while (i < 1) : (i += 0.0001) {
-        const note = 100 * @sin(1382 * @exp((scale['a' % 8 + 1] / 12) * @log(2.0)) * i);
+    const scale = [_]f32{ 0, 2, 3, 5, 7, 8, 10, 12 };
 
-        try stdout.print("{d}\n", .{@floatToInt(i32, note)});
+    const random_buffer = try generateRandom(128);
+
+    std.debug.print("{d}\n", .{random_buffer});
+
+    for (random_buffer) |v| {
+        var i: f64 = 0;
+
+        while (i < 1) : (i += 0.0001) {
+            const note = 100 * @sin(1382 * @exp((scale[v % 8] / 12) * @log(2.0)) * i);
+
+            // try stdout.print("{d}\n", .{@floatToInt(u8, (note * 0.01 * 256 / 2) + 256 / 2)});
+            try list.append(@floatToInt(u8, (note * 0.01 * 256 / 2) + 256 / 2));
+        }
     }
+
+    try wav.Saver(@TypeOf(stdout)).save(stdout, list.toOwnedSlice(), .{
+        .num_channels = 1,
+        .sample_rate = 44100,
+        .format = .unsigned8,
+    });
 }
 
 // const random_buffer = try generateRandom(2);
