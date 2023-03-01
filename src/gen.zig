d7c37daf4d9e670fbde6f4e52fe1912c9f8eac3b
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-03-01 22:52:38 +0300

    refactor(wav): simplify amplitude calculation

--- src/gen.zig
+++ src/gen.zig
@@ -12,8 +12,8 @@ pub fn Generator(comptime scale: []const f32) type {
             var buffer = std.ArrayList(u8).init(allocator);
             var i: f64 = 0;
             while (i < 1) : (i += 0.0001) {
-                const note = @sin(1382 * @exp((scale[sample % scale.len] / 12) * @log(2.0)) * i);
-                try buffer.append(@floatToInt(u8, (note * 256 / 2) + 256 / 2));
+                const amp = @sin(440 * std.math.pi * std.math.pow(f32, 2, scale[sample % scale.len] / 12) * i);
+                try buffer.append(@floatToInt(u8, (amp * 256 / 2) + 256 / 2));
             }
             return buffer.toOwnedSlice();
         }
