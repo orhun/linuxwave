16493b164812f4d418cb1e069f0a1d8d645d8e96
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-03-14 16:43:44 +0300

    chore(build): remove the preferred release mode
    This reverts commit 421ca3abc992ff2388f977418ae4362291ba9b3a.

--- build.zig
+++ build.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
-    // Set the default release mode.
-    b.setPreferredReleaseMode(.ReleaseSafe);
-
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
